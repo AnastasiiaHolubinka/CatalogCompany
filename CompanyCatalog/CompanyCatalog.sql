e92c359edd45ad3f1dba5b2d729a6e66fc7b1109
Anastasiia <38462255+AnastasiiaHolubinka@users.noreply.github.com>
2021-10-25 12:41:34 +0300

    fixed CatalogCompany sql

--- CompanyCatalog/CompanyCatalog.sql
+++ CompanyCatalog/CompanyCatalog.sql
@@ -1,4 +1,4 @@
-﻿CREATE DATABASE CompanyCatalog
+CREATE DATABASE CompanyCatalog
 
 GO
 
@@ -7,62 +7,35 @@ USE CompanyCatalog
 GO
 
 CREATE TABLE [dbo].Company (
-    [Id]          INT           IDENTITY (1, 1) NOT NULL,
-    [Title]       VARCHAR (40)  NOT NULL,
-    [ReleaseYear] INT           NULL,
-    [Genre]       VARCHAR (100) NULL,
-    [Duration]    INT           NULL,
+    [Id]					INT IDENTITY (1, 1) NOT NULL,
+    [CompanyName]			VARCHAR (40)  NOT NULL,
+	[CompanyAddress]        VARCHAR (100)  NOT NULL,
+    [FoundationYear]		INT           NOT NULL,
+	[AnnualRevenue]			INT           NOT NULL,
+    [BusinessType]			VARCHAR (100) NOT NULL,
+
     PRIMARY KEY CLUSTERED ([Id] ASC),
 	CONSTRAINT C_UNIQUE_ID UNIQUE(Id)
 );
 
 GO
 
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Get Out', 2017, N'Thriller', 140)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Dilwale Dulhanya Lejaengy', 1998, N'Romance, Action', 200)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Fast and Furious', 2014, N'Thriller, Action', 140)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Perks of Being a Wall Flower', 2011, N'Romance, Drama', 135)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'SpiderMan: Homecoming', 2017, N'Action, Thriller', 122)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Romeo and Juliet', 1993, N'Romance', 175)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Hotel Transalvinya', 2017, N'Comedy, Drama', 103)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Scent of a Woman', 1996, N'Drama', 140)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Gangster', 2017, N'Horror', 130)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Happy Feet', 2006, N'Animation', 110)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Harry Potter VI - Halbblutprinz', 2009, N'Fantasy', 122)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Hercules',2014, N'Animation', 124)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'I am Legend',2007, N'Action', 128)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Identity', 2003, N'Horror', 127)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Kate & Leopold', 2001, N'Liebe', 150)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Jurassic World',2005, N'Action', 130)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'King Arthur',2004,'Historical', 120)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Laws of Attraction',2004,N'Liebe', 110)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Lion King',1994,N'Animation', 134)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Matrix Revolutions',2003,N'Sci-Fi', 185)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Next', 2007, N'Sci-Fi', 132)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Oblivion', 2013, N'Sci-Fi', 111)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Password Swordfish', 2001, N'Crime', 150)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'S.W.A.T',2003,N'Action', 130)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Half Light', 2006, N'Horror', 120)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Gone Girl', 2014, N'Crime', 110)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Gladiator', 2000, N'Historical', 160)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'From Hell', 2001, N'Fantasy', 180)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Frequency',2000, N'Sci-Fi', 120)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Gattaca',1997,N'Sci-Fi', 110)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Cowboys and Aliens',2011,N'Sci-Fi',100)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Constantine',	2005, N'Fantasy', 90)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Cool Runnings', 1993, N'Komodie', 120)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Avatar', 2009, N'Horror', 120)
-INSERT INTO [dbo].[Company] ([Title], [ReleaseYear], [Genre], [Duration]) VALUES (N'Babel', 2011, N'Drama', 120)
+INSERT INTO [dbo].[Company] ([CompanyName], [CompanyAddress], [FoundationYear], [AnnualRevenue], [BusinessType]) VALUES (N'epam ', N'вул. Угорська, 14', 1993, 12000, N'private')
+INSERT INTO [dbo].[Company] ([CompanyName], [CompanyAddress], [FoundationYear], [AnnualRevenue], [BusinessType]) VALUES (N'softserve ', N'79000, вул. Героїв УПА, 72', 1993, 14000, N'public')
+INSERT INTO [dbo].[Company] ([CompanyName], [CompanyAddress], [FoundationYear], [AnnualRevenue], [BusinessType]) VALUES (N'SOLEAD Software ', N'7D Naukova Str., 79060 ', 2019, 10000, N'non-profit')
+INSERT INTO [dbo].[Company] ([CompanyName], [CompanyAddress], [FoundationYear], [AnnualRevenue], [BusinessType]) VALUES (N'DevRain ', N'вулиця Велика Васильківська, 72, Київ, 03150', 2011, 11000, N'venture capital')
 
 GO
 
 CREATE PROCEDURE [dbo].[addRecord]
-	@pTitle varchar(40),
-	@pReleaseYear int,
-	@pGenre VARCHAR(100),
-	@pDuration int
+	@pCompanyName varchar(40),
+	@pCompanyAddress varchar(100),
+	@pFoundationYear int,
+	@pAnnualRevenue int,
+	@pBusinessType VARCHAR(100)
+
 AS
-	INSERT INTO Company (Title, ReleaseYear, Genre, Duration) VALUES (@pTitle,@pReleaseYear,@pGenre,@pDuration);
+	INSERT INTO Company (CompanyName, CompanyAddress, FoundationYear, AnnualRevenue, BusinessType ) VALUES (@pCompanyName,@pCompanyAddress, @pFoundationYear,@pAnnualRevenue,@pBusinessType);
 
 GO
 
@@ -80,19 +53,20 @@ AS
 	declare @param VARCHAR(40)
 	set @param = '%' + @TitlePhrase + '%' 
 
-	SELECT * FROM Company WHERE Title LIKE @param;
+	SELECT * FROM Company WHERE CompanyName LIKE @param;
 
 GO
 
 CREATE PROCEDURE [dbo].[updateRecord]
 	@pID int,
-	@pTitle varchar(40),
-	@pReleaseYear int,
-	@pGenre VARCHAR(100),
-	@pDuration int
+	@pCompanyName varchar(40),
+	@pCompanyAddress varchar(100),
+	@pFoundationYear int,
+	@pAnnualRevenue int,
+	@pBusinessType VARCHAR(100)
 AS
 	UPDATE Company
-	SET Title = @pTitle, Genre = @pGenre, ReleaseYear = @pReleaseYear, Duration = @pDuration 
+	SET CompanyName = @pCompanyName, CompanyAddress = @pCompanyAddress, FoundationYear = @pFoundationYear, AnnualRevenue = @pAnnualRevenue, BusinessType = @pBusinessType
 	WHERE id = @pID;
 
 GO
